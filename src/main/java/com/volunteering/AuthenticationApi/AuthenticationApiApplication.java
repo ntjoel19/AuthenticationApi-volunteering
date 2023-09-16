fedceeb8bbae80c8be3236cdb366bda147d405aa
Ntepp Jean Joel <ntjoel19@gmail.com>
2023-09-16 18:31:59 +0100

    Initial commit

---
+++ src/main/java/com/volunteering/AuthenticationApi/AuthenticationApiApplication.java
@@ -0,0 +1,13 @@
+package com.volunteering.AuthenticationApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/volunteering/AuthenticationApi/AuthenticationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volunteering.AuthenticationApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
